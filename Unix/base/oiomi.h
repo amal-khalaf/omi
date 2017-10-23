721204bac453869467380e003bf54a93f98d48c0
George Fleming <v-geflem@microsoft.com>
2017-10-23 15:07:19 -0700

    Rebase onto 1.4 and move some preexec code to server/engine

--- Unix/base/oiomi.h
+++ Unix/base/oiomi.h
@@ -2447,18 +2447,6 @@ FILE_EVENT3(40034, trace_Selector_RemoveAllHandlers_Impl, LOG_INFO, PAL_T("Selec
 #endif
 FILE_EVENT1(40035, trace_ServerFailedPamCheckUser_Impl, LOG_INFO, PAL_T("Server failed to authenticate user: (%s)"), const char*)
 #if defined(CONFIG_ENABLE_DEBUG)
-#define trace_SigHUP_received(a0) trace_SigHUP_received_Impl(__FILE__, __LINE__, scs(a0))
-#else
-#define trace_SigHUP_received(a0) trace_SigHUP_received_Impl(0, 0, scs(a0))
-#endif
-FILE_EVENT1(40036, trace_SigHUP_received_Impl, LOG_INFO, PAL_T("SIGHUP received at: (%s)"), const char*)
-#if defined(CONFIG_ENABLE_DEBUG)
-#define trace_SigTERM_received(a0) trace_SigTERM_received_Impl(__FILE__, __LINE__, scs(a0))
-#else
-#define trace_SigTERM_received(a0) trace_SigTERM_received_Impl(0, 0, scs(a0))
-#endif
-FILE_EVENT1(40037, trace_SigTERM_received_Impl, LOG_INFO, PAL_T("SIGTERM received at: (%s)"), const char*)
-#if defined(CONFIG_ENABLE_DEBUG)
 #define trace_FunctionEntered(a0, a1) trace_FunctionEntered_Impl(__FILE__, __LINE__, scs(a0), a1)
 #else
 #define trace_FunctionEntered(a0, a1) trace_FunctionEntered_Impl(0, 0, scs(a0), a1)
